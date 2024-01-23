37e4af2b2ee3e78affb2155757edb4b63b84da6f
machuwey <matvetron@gmail.com>
2024-01-23 14:50:14 +0100

    Add Worldcoin page and API route, update Next.js version

--- src/app/api/verify/route.ts
+++ src/app/api/verify/route.ts
@@ -1,4 +1,10 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
+
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+};
 
 export type VerifyReply = {
   code: string;
@@ -7,9 +13,13 @@ export type VerifyReply = {
 
 const verifyEndpoint = `${process.env.NEXT_PUBLIC_WLD_API_BASE_URL}/api/v1/verify/${process.env.NEXT_PUBLIC_WLD_APP_ID}`;
 
-export async function POST(req: NextRequest) {
-  console.log("Received request to verify credential:\n", req.body);
+export async function POST(
+  req: NextRequest
+) {
   const reqBody = await req.json();
+
+  console.log("Received request to verify credential:\n", reqBody);
+
   const body = {
     nullifier_hash: reqBody.nullifier_hash,
     merkle_root: reqBody.merkle_root,
@@ -18,61 +28,37 @@ export async function POST(req: NextRequest) {
     action: reqBody.action,
     signal: reqBody.signal,
   };
+
   console.log("Sending request to World ID /verify endpoint:\n", body);
 
-  try {
-    const verifyRes = await fetch(verifyEndpoint, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    });
+  const verifyRes = await fetch(verifyEndpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
 
-    if (!verifyRes.ok) {
-      throw new Error(`HTTP error! status: ${verifyRes.status}${verifyRes.statusText} body: ${await verifyRes.text()}`);
-    }
+  const wldResponse = await verifyRes.json();
 
-    const wldResponse: VerifyReply = await verifyRes.json();
+  console.log(
+    `Received ${verifyRes.status} response from World ID /verify endpoint:\n`,
+    wldResponse
+  );
+
+  if (verifyRes.status == 200) {
     console.log(
-      `Received ${verifyRes.status} response from World ID /verify endpoint:\n`,
-      wldResponse
+      "Credential verified! This user's nullifier hash is: ",
+      wldResponse.nullifier_hash
     );
-
-    if (verifyRes.status === 200) {
-      console.log(
-        "Credential verified! This action verified correctly!"
-      );
-      return new NextResponse(JSON.stringify({
-        code: "success",
-        detail: "This action verified correctly!",
-      }), {
-        status: verifyRes.status,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-    } else {
-      return new NextResponse(JSON.stringify({
-        code: wldResponse.code,
-        detail: wldResponse.detail,
-      }), {
-        status: verifyRes.status,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-    }
-  } catch (error) {
-    console.error("Error when calling World ID /verify endpoint:\n", error);
-    return new NextResponse(JSON.stringify({
-      code: "error",
-      detail: "An error occurred while verifying the credential.",
-    }), {
-      status: 500,
-      headers: {
-        'Content-Type': 'application/json',
-      },
+    return NextResponse.json({
+      code: "success",
+      detail: "This action verified correctly!",
+    });
+  } else {
+    return NextResponse.json({
+      code: wldResponse.code,
+      detail: wldResponse.detail,
     });
   }
 }
